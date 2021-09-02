d587fc65c49d8488c903d0d1a918c65a7990cefd
Vishal <vishal.aditya6@gmail.com>
2021-09-02 20:09:53 +0530

    Create Entity add Spring Data JPA and Bootstrap with CommandLine

---
+++ src/main/java/com/example/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,44 @@
+package com.example.spring5webapp.bootstrap;
+
+import com.example.spring5webapp.domain.Author;
+import com.example.spring5webapp.domain.Book;
+import com.example.spring5webapp.repositories.AuthorRepository;
+import com.example.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("No EJB", "323232");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in BootStrap");
+        System.out.println("Number of books " + bookRepository.count());
+
+    }
+}
